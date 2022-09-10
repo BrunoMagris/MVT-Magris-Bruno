dfa5d3da899316e586d5e00e54f5534bf3cd091d
BrunoMagris <brunooem.bm@gmail.com>
2022-09-10 18:03:53 -0300

    Familia con base de datos

--- Familia/AppFamilia/views.py
+++ Familia/AppFamilia/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from AppFamilia.models import Familiares
+from django.http import HttpResponse
 # Create your views here.
+
+def Familia(request):
+
+    familiar = Familiares(nombre="Ismael", apellido = "Magris", edad =9, proximo_cumpleaños = "2023-08-15")
+
+    familiar.save()
+
+    return HttpResponse(f"Estoy guardando a mis familiares {familiar.nombre}")
--- Familia/Familia/urls.py
+++ Familia/Familia/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from AppFamilia.views import Familia
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("familia/", Familia),
 ]
